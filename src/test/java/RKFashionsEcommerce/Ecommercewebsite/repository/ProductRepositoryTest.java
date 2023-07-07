4939036c59a455e6d3d5a7b91d0fb54caebc5c00
santhoshi <skomirelly2@gmail.com>
2023-07-07 18:04:06 -0500

    Product Repo

---
+++ src/test/java/RKFashionsEcommerce/Ecommercewebsite/repository/ProductRepositoryTest.java
@@ -0,0 +1,34 @@
+package RKFashionsEcommerce.Ecommercewebsite.repository;
+import RKFashionsEcommerce.Ecommercewebsite.model.Product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    public void testFindAllByCategory_Id() {
+        // Arrange
+        long categoryId = 1L;
+
+        // Act
+        List<Product> products = productRepository.findAllByCategory_Id(categoryId);
+
+        // Assert
+        assertEquals(1, products.size());
+        assertNotNull(products.get(0));
+
+        // Additional assertions...
+    }
+}
+
